73080134d3b6839fa39930275206ab2c084fd258
Mamad <mamadbosqests@gmail.com>
2021-10-27 23:22:46 +0330

    learn date in js

---
+++ date/date.js
@@ -0,0 +1,11 @@
+let date = new Date();
+// let date = new Date("October 6 2000 14:20:11");
+
+console.log(`Year : ${date.getFullYear()}`)
+console.log(`Month : ${date.getMonth()}`)
+console.log(`Day : ${date.getDay()}`)
+console.log(`Hours : ${date.getHours()}`)
+console.log(`Minute : ${date.getMinutes()}`)
+console.log(`Second : ${date.getSeconds()}`)
+
+console.log(date.getTime())
