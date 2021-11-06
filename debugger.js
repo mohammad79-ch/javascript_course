3b1a1c905b0ce04592ac5cf1fceccadd32a107de
Mamad <mamadbosqests@gmail.com>
2021-11-06 23:50:16 +0330

    fix the hangman game

--- debugger.js
+++
@@ -1,7 +0,0 @@
-function person(){
-    return "mamad"
-}
-
-mamad();
-
-// f12 -> source -> open current page -> show result
--- hangmans/hangman.js
+++ hangmans/hangman.js
@@ -1,25 +1,40 @@
 const Hangman = function (word,limit){
     this.word =  word.toLowerCase().split('');
     this.limit = limit;
+    this.guessesWord = [];
     this.showWordPara = document.getElementById("guessWord");
 }
 
-Hangman.prototype.getpuzzle = function (guessesWord){
+Hangman.prototype.getpuzzle = function (){
     let puzzle = "";
 
     this.word.forEach((letter)=>{
-        if (guessesWord.includes(letter) || letter === " "){
+        if (this.guessesWord.includes(letter) || letter === " "){
             puzzle += letter;
         }else{
             puzzle += "*";
         }
     })
-    console.log(puzzle)
-    console.log(this.limit)
 
     this.showWordPara.textContent = puzzle;
+
+    return puzzle;
 }
 
+Hangman.prototype.getgusses = function (guess){
+
+    let isUnique = !this.guessesWord.includes(guess);
+    if (isUnique){
+        game.guessesWord.push(guess)
+    }
+
+    if (isUnique && !game.word.includes(guess)){
+        this.limit--;
+    }
+
+    console.log(this.limit)
+
+}
 
 const game = new Hangman("Mohammad",2);
 
@@ -30,18 +45,11 @@ window.addEventListener("DOMContentLoaded",function (){
 
 });
 
-document.getElementById("hangmanInput").addEventListener("input",function (e){
+document.getElementById("hangmanInput").addEventListener("keypress",function (e){
 
-    if (game.limit === 0){
-        document.getElementById("wrong").style.display = "block";
-        game.showWordPara.textContent = game.word;
+    game.getgusses(e.key)
 
-        setTimeout(function (){
-            location.reload();
-        },2000)
-        return 0;
-    }
-    game.getpuzzle(e.target.value);
-
+    console.log(game.getpuzzle())
 })
+
 
