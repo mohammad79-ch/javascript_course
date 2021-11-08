91e3a5f5a2fff7ab82989f06a26b39700266c01e
Mamad <mamadbosqests@gmail.com>
2021-11-08 23:26:28 +0330

    chnage the hole hangman game to class base

--- hangmans/hangman.js
+++ hangmans/hangman.js
@@ -1,38 +1,40 @@
-const Hangman = function (word,limit){
+class Hangman {
+
+    constructor(word,limit) {
+
     this.word =  word.toLowerCase().split('');
     this.limit = limit;
     this.guessesWord = [];
     this.showWordPara = document.getElementById("guessWord");
     this.showLimitOnPage = document.getElementById("showLimitOnPage");
-}
+    }
 
-Hangman.prototype.getpuzzle = function (){
-    let puzzle = "";
+    getpuzzle(){
+        let puzzle = "";
 
-    this.word.forEach((letter)=>{
-        if (this.guessesWord.includes(letter) || letter === " "){
-            puzzle += letter;
-        }else {
-            puzzle += "*";
+        this.word.forEach((letter)=>{
+            if (this.guessesWord.includes(letter) || letter === " "){
+                puzzle += letter;
+            }else {
+                puzzle += "*";
+            }
+        })
+
+        this.showWordPara.textContent = puzzle;
+
+        return puzzle;
+    }
+
+    getgusses(guess){
+        let isUnique = !this.guessesWord.includes(guess);
+        if (isUnique){
+            game.guessesWord.push(guess)
         }
-    })
 
-    this.showWordPara.textContent = puzzle;
-
-    return puzzle;
-}
-
-Hangman.prototype.getgusses = function (guess){
-
-    let isUnique = !this.guessesWord.includes(guess);
-    if (isUnique){
-        game.guessesWord.push(guess)
+        if (isUnique && !game.word.includes(guess)){
+            this.limit--;
+        }
     }
-
-    if (isUnique && !game.word.includes(guess)){
-        this.limit--;
-    }
-
 
 }
 
@@ -44,7 +46,6 @@ window.addEventListener("DOMContentLoaded",function (){
     }
 
     game.showLimitOnPage.textContent = game.limit;
-
 
 });
 
