18a17803c7422ed380630879c76de9cb7cca5651
Mamad <mamadbosqests@gmail.com>
2021-12-11 14:12:49 +0330

    fix the slider image  project

--- practice-project/image-slider/image-slider.js
+++ practice-project/image-slider/image-slider.js
@@ -3,37 +3,27 @@ const sliderImages = document.querySelectorAll(".image-slider img");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 
-let counter = 1;
+let counter = 0;
 let size = sliderImages[0].clientWidth;
 
 // slideCarouel.style.transform = 'translateX(' + (-size * counter)+ 'px)';
 
-nextBtn.addEventListener("click",(e)=>{
-    if (counter >= sliderImages.length - 1)return
+nextBtn.addEventListener("click",(e)=> {
+    if (counter >= sliderImages.length - 1) {
+        counter = 0
+    }else {
+        counter++
+    }
     slideCarouel.style.transition = 'transform 0.4s ease-in-out'
-    counter++
     slideCarouel.style.transform = 'translateX(' + (-size * counter)+ 'px)';
 })
 
 prevBtn.addEventListener("click",(e)=>{
-    if (counter <=0)return
-    slideCarouel.style.transition = 'transform 0.4s ease-in-out'
+    if (counter <=0){
+        counter = sliderImages.length - 1
+    }else {
     counter--
+    }
+    slideCarouel.style.transition = 'transform 0.4s ease-in-out'
     slideCarouel.style.transform = 'translateX(' + (-size * counter)+ 'px)';
-})
-
-slideCarouel.addEventListener("transitionend",()=>{
-
-    if (sliderImages[counter].id === 'lastClone'){
-        slideCarouel.style.transition = 'none'
-        counter = sliderImages.length - 2
-        slideCarouel.style.transform = 'translateX(' + (-size * counter)+ 'px)';
-    }
-
-    if (sliderImages[counter].id === 'firstClone'){
-        slideCarouel.style.transition = 'none'
-        counter = sliderImages.length - counter
-        slideCarouel.style.transform = 'translateX(' + (-size * counter)+ 'px)';
-    }
-
 })
