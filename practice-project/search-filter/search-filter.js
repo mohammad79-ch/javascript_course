4dfa4b6ed015c79c422a6fe69b59c425b1ac092d
Mamad <mamadbosqests@gmail.com>
2021-12-27 17:57:11 +0330

    validate input

--- practice-project/search-filter/search-filter.js
+++ practice-project/search-filter/search-filter.js
@@ -1,6 +1,7 @@
 const search = document.getElementById("search");
 const content_list = document.querySelector(".content-list");
 let tableList = document.getElementById("table_list");
+let registerData = document.getElementById("registerData");
 
 
 const list = [
@@ -36,28 +37,28 @@ const list = [
     }
 ];
 
-document.addEventListener("DOMContentLoaded",()=>{
-
-    list.forEach((item)=>{
+function shareDataInView() {
+    list.forEach((item) => {
         tableList.innerHTML += `
            <tr>
            <td>${item.name}</td>
            <td>${item.family}</td>
            <td>${item.age}</td>
            </tr>
-
         `
     })
+}
 
+document.addEventListener("DOMContentLoaded",()=>{
+    shareDataInView();
 })
 
 search.addEventListener("input", (e) => {
-
     let searchValue = e.target.value;
     tableList.innerHTML = "";
     let filter = list.filter((item)=>{
         return item.name.toLowerCase().includes(searchValue.toLowerCase());
-    })
+    });
 
     filter.forEach((item)=>{
         tableList.innerHTML += `
@@ -66,9 +67,32 @@ search.addEventListener("input", (e) => {
            <td>${item.family}</td>
            <td>${item.age}</td>
            </tr>
-
         `
     })
+});
 
+
+
+
+registerData.addEventListener("click",(e)=>{
+    let nameInput = document.getElementById("nameInput").value.trim();
+    let familyInput = document.getElementById("famlilyInput").value.trim();
+    let ageSelect = document.getElementById("ageSelect").value.trim();
+
+    validateData(nameInput,familyInput,ageSelect);
 
 });
+
+
+function validateData(nameInput, familyInput, ageSelect) {
+    if (nameInput === ""){
+      alert("here i")
+    }
+
+    if (familyInput === ""){
+      alert("here f")
+    }
+
+
+}
+
